0716a83af5365338a136fe6c286954c5b751d943
KomahidzeGeorgij <komahidzegeorgij@gmail.com>
2024-10-22 15:49:17 +0300

    добавлены 2 переменные ссылка на веб и ссылка на группу

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 TOKEN = '7517001534:AAHI-7quMrsy8eN_KsMS_QAJ3-cjZvMWOFc'
 GROUP_CHAT_ID = -1002446066950
+VEBINAR = 'https://www.youtube.com/watch?v=d3ql-gGtDmk'
+GROUPSHARE = 'https://t.me/+fxtsvv4TQ0lkYTgy'
